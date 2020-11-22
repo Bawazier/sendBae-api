d4a1e1350766788e8e184dca1ba317cb764325af
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-22 19:47:25 +0700

    fix input validation schema

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,24 +3,19 @@ const Joi = require("joi");
 module.exports = {
   phoneValidation: Joi.object({
     countryId: Joi.number().integer().positive(),
-    phoneNumber: Joi.number()
-      .integer()
-      .min(1000000000)
-      .max(99999999999),
+    phoneNumber: Joi.number().integer().min(1000000000).max(99999999999),
   }),
   user: Joi.object({
     username: Joi.string().pattern(new RegExp("^[a-z0-9]{3,30}$")).min(5),
-    firstName: Joi.string().min(3).max(80),
-    lastName: Joi.string().min(3).max(80),
+    firstName: Joi.string().max(80),
+    lastName: Joi.string().max(80),
     bio: Joi.string().max(70),
   }),
   contact: Joi.object({
-    firstName: Joi.string().min(3).max(80),
-    lastName: Joi.string().min(3).max(80),
-    phoneNumber: Joi.number()
-      .integer()
-      .min(1000000000)
-      .max(99999999999),
+    firstName: Joi.string().max(80),
+    lastName: Joi.string().max(80),
+    countryId: Joi.number().integer().positive(),
+    phoneNumber: Joi.number().integer().min(1000000000).max(99999999999),
   }),
   message: Joi.string(),
 };

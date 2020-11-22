22148cb739c0317fad7c4f4151fbc534f878bd63
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-22 10:59:41 +0700

    create auth route

---
+++ src/controllers/auth.js
@@ -0,0 +1,52 @@
+const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+const schema = require("../helpers/validation");
+const responseStandart = require("../helpers/response");
+
+const phoneValidation = schema.phoneValidation;
+
+module.exports = {
+  signIn: async (req, res) => {
+    try {
+      const body = await phoneValidation.required().validateAsync(req.body);
+      const dataUser = {countryId: body.countryId, phoneNumber: body.phoneNumber};
+      const validate = await User.findAll({
+        where: {
+          countryId: body.countryId,
+          phoneNumber: body.phoneNumber,
+        },
+      });
+      if (validate.length) {
+        jwt.sign(
+          { id: validate[0].id },
+          process.env.APP_KEY,
+          { expiresIn: "2 days" },
+          function (err, token) {
+            if (!err) {
+              return responseStandart(res, "Signin Success", {
+                token: token
+              });
+            } else {
+              return responseStandart(res, err, {}, 403, false);
+            }
+          }
+        );
+      } else {
+        const results = await User.create(dataUser);
+        if(results){
+          return responseStandart(res, "Signup Success", {});
+        }else{
+          return responseStandart(
+            res,
+            "Unfortunately this phone number has not supported sendBae yet",
+            {},
+            400,
+            false
+          );
+        }
+      }
+    } catch (e) {
+      return responseStandart(res, e, {}, 400, false);
+    }
+  },
+};

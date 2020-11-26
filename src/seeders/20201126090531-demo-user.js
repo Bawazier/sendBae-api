355413952ad1fed3aa2a33d1658bd05bf8136847
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-26 16:29:51 +0700

    create seeders for user oficial

---
+++ src/seeders/20201126090531-demo-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert("Users", [
+      {
+        username: "sendbae",
+        firstName: "SendBae",
+        lastName: "Official",
+        countryId: 1,
+        phoneNumber: 1212121212,
+        bio: "Send Bae Official",
+        photo: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
